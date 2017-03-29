46231ac7f764860fe304aca3d7d3ef9ce73b484a
oleg.korneychuk <oleg.korneychuk@engagepoint.com>
2017-03-29 14:36:51 -0400

    SONAR Suppress Security - Unvalidated Redirect

--- csrfguard/src/main/java/org/owasp/csrfguard/action/Redirect.java
+++ csrfguard/src/main/java/org/owasp/csrfguard/action/Redirect.java
@@ -41,6 +41,8 @@ public final class Redirect extends AbstractAction {
 	private static final long serialVersionUID = -2265693822259717332L;
 
 	@Override
+	//parameter comes from property file that holds on server side
+	@SuppressWarnings("findsecbugs:UNVALIDATED_REDIRECT")
 	public void execute(HttpServletRequest request, HttpServletResponse response, CsrfGuardException csrfe, CsrfGuard csrfGuard) throws CsrfGuardException {
 		String errorPage = getParameter("Page");
 
